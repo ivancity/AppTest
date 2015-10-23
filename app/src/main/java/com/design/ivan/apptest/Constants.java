0ebadbbdc0bd3abe69196ba0fd5c1b15746c25f6
ivancity <shieldm@hotmail.com>
2015-10-22 19:47:25 -0500

    Sync Adapter now downloads content from the server and attempts to insert it to the data base.

---
+++ app/src/main/java/com/design/ivan/apptest/Constants.java
@@ -0,0 +1,9 @@
+package com.design.ivan.apptest;
+
+/**
+ * Created by ivanm on 10/22/15.
+ */
+public class Constants {
+    public static final String category_url= "http://public.dawanda.in/category.json";
+    public static final String product_url = "http://public.dawanda.in/products.json";
+}
